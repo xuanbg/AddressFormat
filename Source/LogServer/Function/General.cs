9e918efbfa6471feb7676a4708f63f098d707226
xan.brian <xuan_bingan@hotmail.com>
2016-01-11 13:15:37 +0800

    new

---
+++ Source/LogServer/Function/General.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.ServiceModel.Web;
+using System.Text;
+using Insight.WS.Log.Entity;
+using static Insight.WS.Log.Util;
+
+namespace Insight.WS.Log
+{
+    public class General
+    {
+
+        /// <summary>
+        /// 通过Session校验是否有权限访问
+        /// </summary>
+        /// <param name="session">用户会话</param>
+        /// <param name="aid">操作ID</param>
+        /// <returns></returns>
+        public static JsonResult Verify(out Session session, string aid)
+        {
+            session = GetAuthorization<Session>();
+            var result = new JsonResult();
+            return session == null ? result.InvalidAuth() : Verification(session, aid);
+        }
+
+        /// <summary>
+        /// 通过指定的Rule校验是否有权限访问
+        /// </summary>
+        /// <returns>JsonResult</returns>
+        public static JsonResult Verify(string rule)
+        {
+            var result = new JsonResult();
+            var obj = GetAuthorization<string>();
+            return obj != Hash(rule) ? result.InvalidAuth() : result.Success();
+        }
+
+        /// <summary>
+        /// 会话合法性验证，用于持久化客户端
+        /// </summary>
+        /// <param name="obj">用户会话</param>
+        /// <param name="aid">操作ID</param>
+        /// <returns>JsonResult</returns>
+        public static JsonResult Verification(Session obj, string aid)
+        {
+            var url = Address + "verify/auth";
+            var data = $"action={aid}";
+            var author = Base64(obj);
+            return HttpRequest(url, "GET", author, data);
+        }
+
+        /// <summary>
+        /// 获取Authorization承载的数据
+        /// </summary>
+        /// <typeparam name="T">数据类型</typeparam>
+        /// <returns>数据对象</returns>
+        public static T GetAuthorization<T>()
+        {
+            var woc = WebOperationContext.Current;
+            if (woc == null) return default(T);
+
+            var headers = woc.IncomingRequest.Headers;
+            if (!CompareVersion(headers))
+            {
+                woc.OutgoingResponse.StatusCode = HttpStatusCode.NotAcceptable;
+                return default(T);
+            }
+
+            var auth = headers[HttpRequestHeader.Authorization];
+            if (string.IsNullOrEmpty(auth))
+            {
+                woc.OutgoingResponse.StatusCode = HttpStatusCode.Unauthorized;
+                return default(T);
+            }
+
+            var response = woc.OutgoingResponse;
+            response.Headers[HttpResponseHeader.ContentEncoding] = "gzip";
+            response.ContentType = "application/x-gzip";
+
+            try
+            {
+                var buffer = Convert.FromBase64String(auth);
+                var json = Encoding.UTF8.GetString(buffer);
+                return Deserialize<T>(json);
+            }
+            catch (Exception ex)
+            {
+                var log = new SYS_Logs
+                {
+                    Code = "300001",
+                    Level = 3,
+                    Source = "日志服务",
+                    Action = "身份验证",
+                    Message = ex.ToString(),
+                    CreateTime = DateTime.Now
+                };
+                DataAccess.WriteToFile(log);
+                woc.OutgoingResponse.StatusCode = HttpStatusCode.Unauthorized;
+                return default(T);
+            }
+        }
+
+        /// <summary>
+        /// HttpRequest方法
+        /// </summary>
+        /// <param name="url">请求的地址</param>
+        /// <param name="method">请求的方法：GET,PUT,POST,DELETE</param>
+        /// <param name="author">接口认证数据</param>
+        /// <param name="data">接口参数</param>
+        /// <returns>JsonResult</returns>
+        public static JsonResult HttpRequest(string url, string method, string author, string data = "")
+        {
+            if (method == "GET") url += (data == "" ? "" : "?") + data;
+            var request = GetWebRequest(url, method, author);
+
+            if (method == "GET") return GetResponse(request);
+
+            var buffer = Encoding.UTF8.GetBytes(data);
+            request.ContentLength = buffer.Length;
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(buffer, 0, buffer.Length);
+            }
+
+            return GetResponse(request);
+        }
+
+        /// <summary>
+        /// 获取WebRequest对象
+        /// </summary>
+        /// <param name="url">请求的地址</param>
+        /// <param name="method">请求的方法：GET,PUT,POST,DELETE</param>
+        /// <param name="author">接口认证数据</param>
+        /// <returns>HttpWebRequest</returns>
+        private static HttpWebRequest GetWebRequest(string url, string method, string author)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = method;
+            request.ContentType = "application/json";
+            if (author != null)
+            {
+                request.Headers.Add(HttpRequestHeader.Authorization, author);
+            }
+
+            return request;
+        }
+
+        /// <summary>
+        /// 获取Request响应数据
+        /// </summary>
+        /// <param name="request">WebRequest</param>
+        /// <returns>JsonResult</returns>
+        private static JsonResult GetResponse(WebRequest request)
+        {
+            try
+            {
+                var response = (HttpWebResponse)request.GetResponse();
+                var responseStream = response.GetResponseStream();
+                if (responseStream == null) return null;
+
+                using (var reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8")))
+                {
+                    var result = reader.ReadToEnd();
+                    responseStream.Close();
+                    return Deserialize<JsonResult>(result);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                var result = new JsonResult
+                {
+                    Code = "400",
+                    Name = "BadRequest",
+                    Message = ex.ToString()
+                };
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// 验证版本是否兼容
+        /// </summary>
+        /// <param name="headers"></param>
+        /// <returns></returns>
+        private static bool CompareVersion(WebHeaderCollection headers)
+        {
+            var accept = headers[HttpRequestHeader.Accept].Split(Convert.ToChar(";"));
+            if (accept.Length < 2) return false;
+
+            var ver = Convert.ToInt32(accept[1].Substring(9));
+            return ver >= Convert.ToInt32(CompatibleVersion) && ver <= Convert.ToInt32(UpdateVersion);
+        }
+
+    }
+}
---
+++ Source/LogServer/Function/Util.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Web.Script.Serialization;
+using System.Xml;
+using System.Xml.Serialization;
+using Insight.WS.Log.Entity;
+
+namespace Insight.WS.Log
+{
+    public static class Util
+    {
+
+        #region 成员属性
+
+        /// <summary>
+        /// 安全码
+        /// </summary>
+        public const string Secret = "842A381C91CE43A98720825601C22A56";
+
+        /// <summary>
+        /// 访问验证服务用的Address
+        /// </summary>
+        public static readonly string Address = GetAppSetting("BaseServer");
+
+        /// <summary>
+        /// 日志规则列表
+        /// </summary>
+        public static List<SYS_Logs_Rules> Rules;
+
+        /// <summary>
+        /// 进程同步基元
+        /// </summary>
+        public static readonly Mutex Mutex = new Mutex();
+
+        /// <summary>
+        /// 接口最后兼容版本
+        /// </summary>
+        public static readonly string CompatibleVersion = GetAppSetting("CompatibleVersion");
+
+        /// <summary>
+        /// 接口最新版本
+        /// </summary>
+        public static readonly string UpdateVersion = GetAppSetting("UpdateVersion");
+
+        #endregion
+
+        #region 常用静态方法
+
+        /// <summary>
+        /// 读取配置项的值
+        /// </summary>
+        /// <param name="key">配置项</param>
+        /// <returns>配置项的值</returns>
+        public static string GetAppSetting(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+
+        /// <summary>
+        /// 计算字符串的Hash值
+        /// </summary>
+        /// <param name="str">输入字符串</param>
+        /// <returns>String Hash值</returns>
+        public static string Hash(string str)
+        {
+            var md5 = MD5.Create();
+            var s = md5.ComputeHash(Encoding.UTF8.GetBytes(str.Trim()));
+            return s.Aggregate("", (current, c) => current + c.ToString("X2"));
+        }
+
+        /// <summary>
+        /// 生成用于接口验证的Authorization字符串
+        /// </summary>
+        /// <typeparam name="T">输入类型</typeparam>
+        /// <param name="obj">用于接口验证的数据对象</param>
+        /// <returns>string 用于接口验证的Authorization字符串</returns>
+        public static string Base64<T>(T obj)
+        {
+            var json = Serialize(obj);
+            var buff = Encoding.UTF8.GetBytes(json);
+            return Convert.ToBase64String(buff);
+        }
+
+        #endregion
+
+        #region Serialize/Deserialize
+
+        /// <summary>
+        /// 将一个对象序列化为Json字符串
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="obj">对象</param>
+        /// <returns>string Json字符串</returns>
+        public static string Serialize<T>(T obj)
+        {
+            return new JavaScriptSerializer().Serialize(obj);
+        }
+
+        /// <summary>
+        /// 将一个Json字符串反序列化为指定类型的对象
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="json">Json字符串</param>
+        /// <returns>T 反序列化的对象</returns>
+        public static T Deserialize<T>(string json)
+        {
+            return new JavaScriptSerializer().Deserialize<T>(json);
+        }
+
+        /// <summary>
+        /// 将一个对象序列化为XML字符串
+        /// </summary>
+        /// <param name="obj">要序列化的对象</param>
+        /// <param name="encoding">编码方式</param>
+        /// <returns>序列化产生的XML字符串</returns>
+        public static string Serialize(object obj, Encoding encoding)
+        {
+            using (var stream = new MemoryStream())
+            {
+                SerializeInternal(stream, obj, encoding);
+
+                stream.Position = 0;
+                using (var reader = new StreamReader(stream, encoding))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 从XML字符串中反序列化对象
+        /// </summary>
+        /// <typeparam name="T">结果对象类型</typeparam>
+        /// <param name="xml">包含对象的XML字符串</param>
+        /// <param name="encoding">编码方式</param>
+        /// <returns>反序列化得到的对象</returns>
+        public static T Deserialize<T>(string xml, Encoding encoding)
+        {
+            using (var ms = new MemoryStream(encoding.GetBytes(xml)))
+            {
+                using (var sr = new StreamReader(ms, encoding))
+                {
+                    var mySerializer = new XmlSerializer(typeof(T));
+                    return (T)mySerializer.Deserialize(sr);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 序列化预处理
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="obj"></param>
+        /// <param name="encoding"></param>
+        private static void SerializeInternal(Stream stream, object obj, Encoding encoding)
+        {
+            var serializer = new XmlSerializer(obj.GetType());
+            var settings = new XmlWriterSettings
+            {
+                Indent = true,
+                NewLineChars = "\r\n",
+                Encoding = encoding,
+                IndentChars = "    "
+            };
+
+            using (var writer = XmlWriter.Create(stream, settings))
+            {
+                serializer.Serialize(writer, obj);
+            }
+        }
+
+        #endregion
+
+        #region Compress/Decompress
+
+        /// <summary>
+        /// GZip压缩
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static byte[] Compress(byte[] data)
+        {
+            var ms = new MemoryStream();
+            var stream = new GZipStream(ms, CompressionMode.Compress, true);
+            stream.Write(data, 0, data.Length);
+            stream.Close();
+            return ms.ToArray();
+        }
+
+        /// <summary>
+        /// ZIP解压
+        /// </summary>
+        /// <param name="dada"></param>
+        /// <returns></returns>
+        public static byte[] Decompress(byte[] dada)
+        {
+            var ms = new MemoryStream(dada);
+            var stream = new GZipStream(ms, CompressionMode.Decompress);
+            var buffer = new MemoryStream();
+            var block = new byte[1024];
+            while (true)
+            {
+                var read = stream.Read(block, 0, block.Length);
+                if (read <= 0) break;
+                buffer.Write(block, 0, read);
+            }
+            stream.Close();
+            return buffer.ToArray();
+        }
+
+        #endregion
+
+    }
+}
